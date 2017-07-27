2f12a0e187fb5e887fe9b5abfea6683d2f1011d9
Abraham Kwak <kwak.abe@gmail.com>
2017-07-27 15:08:32 -0400

    Refactor model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
-  has_many :questions
-  has_many :answers
-  has_many :comments
-  has_many :votes
+  has_many :questions, :foreign_key => :creator
+  has_many :answers, :foreign_key => :responder
+  has_many :comments, :foreign_key => :commenter
+  has_many :votes, :foreign_key => :voter
 
   has_secure_password
 
