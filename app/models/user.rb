dc3024308aeec1c1408fb6a83fe23cbf70596dad
Abraham Kwak <kwak.abe@gmail.com>
2017-07-27 15:13:28 -0400

    Successful associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
-  has_many :questions, :foreign_key => :creator
-  has_many :answers, :foreign_key => :responder
-  has_many :comments, :foreign_key => :commenter
-  has_many :votes, :foreign_key => :voter
+  has_many :questions, :foreign_key => :creator_id
+  has_many :answers, :foreign_key => :responder_id
+  has_many :comments, :foreign_key => :commenter_id
+  has_many :votes, :foreign_key => :voter_id
 
   has_secure_password
 
