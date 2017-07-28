847eeee60619cf0d8d13f63999c778aab279dedb
Rakey Drammeh <rdrammeh@gmail.com>
2017-07-28 12:11:17 -0400

    Handle error on comments route

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -1,12 +1,12 @@
 post '/comments/:answer_id' do
   #:id here is the answer id
-  answer = Answer.find_by(id: params[:answer_id])
+  @answer = Answer.find_by(id: params[:answer_id])
   comment = Comment.new(body: params[:body], commenter: current_user.id, commentable: @answer)
   if comment.save
-    redirect "/questions/#{answer.question_id}"
+    redirect "/questions/#{@answer.question_id}"
   else
     @errors = ["Comment could not be saved", "Please try again"]
-    redirect "/questions/#{answer.question_id}"
+    redirect "/questions/#{@answer.question_id}"
   end
 end
 
